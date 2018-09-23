04ee3f4be9f6ba2c981f8a062213a13bf3d7bd0b
Andrew Lange <andrew.lange93@gmail.com>
2018-09-23 17:42:16 -0400

    Import database created in models.py.

--- votr/votr/votr.py
+++ votr/votr/votr.py
@@ -1,6 +1,17 @@
+# import flask microservice. #
 from flask import Flask
+# import data model. #
+from models import db
 
+# initialize Flask object . #
 votr = Flask(__name__)
+
+# load config from the config file we created earlier. #
+votr.config.from_object('config')
+
+# initialize and create the database. #
+db.init_app(votr)
+db.create_all(app=votr)
 
 @votr.route('/')
 def home():
